80d9b01a44c3865664ffe45e9df66843697f0864
m0nstah <saranprasad97@gmail.com>
2020-03-07 13:14:55 +0530

    cors for dev

--- index.js
+++ index.js
@@ -11,6 +11,11 @@ api.mob = apiMob;
 
 // Express use utilities
 app.use(bodyParser.json());
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "http://localhost:4200"); // update to match the domain you will make the request from
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
 app.use('/mob', api.mob);
 
 app.get('/test', function(req, res) {
