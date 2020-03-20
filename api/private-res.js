661dacdeb1fbcebd993c578dc43b17680f9d5514
m0nstah <saranprasad97@gmail.com>
2020-03-20 13:39:23 +0530

    mob apis

--- api/private-res.js
+++ api/private-res.js
@@ -7,11 +7,26 @@ router.use(bodyParser.json());
 
 
 // Router routes
-router.get('/dp', function (req, res) {
+router.get('/getDp', function (req, res) {
     // Get dp path from db
     // let path = '';
-    res.sendFile('dp.jpg', { root: config.privateResConfig.path });
+    res.sendFile('vim_avatar.jpg', { root: config.privateResConfig.userPath });
     // Send dp
+});
+
+router.get('/getSubjects', function (req, res) {
+    let subjects = [
+        { name: 'database management', url: '/mob/private/getSubImg/dbms' },
+		{ name: 'computer network', url: '/mob/private/getSubImg/cn' },
+		{ name: 'cryptography', url: '/mob/private/getSubImg/crypto' },
+		{ name: 'machine learning', url: '/mob/private/getSubImg/ml' }
+	];
+
+    res.json(subjects);
+});
+
+router.get('/getSubImg/:subId', function (req, res) {
+    res.sendFile(req.params.subId + '.jpg', { root: config.privateResConfig.subPath });
 });
 
 
